2737c871685d97d6baeeb3c6e3e2794fccc8a8f7
RethinkitSupport <134937688+RethinkitSupport@users.noreply.github.com>
2024-10-12 22:53:55 -0400

    Update screenconnect_install.ps1

--- screenconnect_install.ps1
+++ screenconnect_install.ps1
@@ -1,4 +1,4 @@
-$url = "https://rethinkit2410101053.screenconnect.com/Bin/ScreenConnect.ClientSetup.exe?e=Access&y=Guest&c=Rethinkit%20Internal&c=&c=&c=&c=&c=&c=&c="
+$url = "https://rethinkit2410101053.screenconnect.com/Bin/ScreenConnect.ClientSetup.exe?e=Access&y=Guest&c=Unassigned&c=&c=&c=&c=&c=&c=&c="
 $outpath = "C:\sc.exe"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $outpath)
