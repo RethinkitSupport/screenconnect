513f8e9d9fd0e9da9add5434a9b8842331d3197f
RethinkitSupport <134937688+RethinkitSupport@users.noreply.github.com>
2024-10-14 10:22:37 -0400

    Update screenconnect_install.ps1

--- screenconnect_install.ps1
+++ screenconnect_install.ps1
@@ -1,8 +1,8 @@
 Write-Host "Installing ScreenConnect..." -ForegroundColor Green
 $url = "https://rethinkit2410101053.screenconnect.com/Bin/ScreenConnect.ClientSetup.exe?e=Access&y=Guest&c=Unassigned&c=&c=&c=&c=&c=&c=&c="
-$outpath = "$($env:TEMP)\sc.exe"
+$outpath = "$($env:TEMP)\ScreenConnectInstaller.exe"
 $wc = New-Object System.Net.WebClient
-Write-Host "Downloading..."
+Write-Host "Downloading ($($outpath))..."
 $wc.DownloadFile($url, $outpath)
 Write-Host "Installing..."
 Start-Process -Filepath $outpath -Wait
