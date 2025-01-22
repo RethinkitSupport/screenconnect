a6fc2fcb5aa321b5164303e365775ca5bb8ee7b2
RethinkitSupport <134937688+RethinkitSupport@users.noreply.github.com>
2025-01-22 08:12:49 -0500

    Add files via upload

--- screenconnect_install.ps1
+++ screenconnect_install.ps1
@@ -2,15 +2,13 @@
 # This script has been posted to Rethinkit's GitHub for easy deployment
 #
 # To install Press Windows+R and paste this
-# powershell -executionpolicy bypass Invoke-Expression (Invoke-WebRequest https://raw.githubusercontent.com/RethinkitSupport/pcinfo/screenconnect/screenconnect_install.ps1 -UseBasicParsing).Content
-#
+# powershell -executionpolicy bypass Invoke-Expression (Invoke-WebRequest https://raw.githubusercontent.com/RethinkitSupport/screenconnect/main/screenconnect_install.ps1 -UseBasicParsing).Content
 ###
 Param ## provide a comma separated list of switches
 	(
 	[string] $mode = "auto" #auto or manual
 	)
 $mode_auto = ($mode -eq "auto")
-
 Function IsAdmin() 
 {
     $wid=[System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -19,12 +17,8 @@ Function IsAdmin()
     $IsAdmin=$prp.IsInRole($adm)
     $IsAdmin
 }
-
 ###
-$scriptFullname = $PSCommandPath ; if (!($scriptFullname)) {$scriptFullname =$MyInvocation.InvocationName }
-$scriptDir      = Split-Path -Path $scriptFullname -Parent
-$scriptName     = Split-Path -Path $scriptFullname -Leaf
-if ((Test-Path("$scriptDir\ITAutomator.psm1"))) {Import-Module "$scriptDir\ITAutomator.psm1" -Force} else {write-host "Err 99: Couldn't find ITAutomator.psm1";Start-Sleep -Seconds 10;Exit(99)}
+$scriptName     = "screen_connect_install.ps1"
 $UrlHost  = "rethinkit"
 $UrlBase  = "https://$($UrlHost).screenconnect.com/Bin/ScreenConnect.ClientSetup.exe"
 $Company    = "Rethinkit"
@@ -44,42 +38,41 @@ Write-host "      Dept: $($Dept)"
 Write-host "DeviceType: $($DeviceType)"
 Write-host ""
 Write-Host "-----------------------------------------------------------------------------"
-if ($mode_auto) {PauseTimed -quiet} else {PauseTimed}
+if ($mode_auto) {Write-Host "Auto-install"} else {Pause}
 If (-not (Isadmin))
 {
 	Write-Host "Requires elevation.  Re-run as admin."
 	start-sleep 3
 	exit 99
 }
-#########
-$tmpFile = Split-Path $UrlBase -leaf
-###
+$exitcode=0
+# Convert settings to URL format
 $Company    = [System.Web.HttpUtility]::UrlEncode($Company)    -replace '\+', '%20'
 $Site       = [System.Web.HttpUtility]::UrlEncode($Site)       -replace '\+', '%20'
 $Dept       = [System.Web.HttpUtility]::UrlEncode($Dept)       -replace '\+', '%20'
 $DeviceType = [System.Web.HttpUtility]::UrlEncode($DeviceType) -replace '\+', '%20'
-###
+# Get a temp file to download to
+$tmpFile = Join-Path $Env:Temp (Split-Path $UrlBase -leaf)
+# Get the download path
 $WebUrl  = "$($UrlBase)?e=Access&y=Guest&c=$($Company)&c=$($Site)&c=$($Department)&c=$($DeviceType)&c=&c=&c=&c="
-####
-$exitcode=0
-$tmpFile=Join-Path $Env:Temp $tmpFile
-
+# Download (without progress / faster)
 $Pp_old=$ProgressPreference;$ProgressPreference = 'SilentlyContinue' # Change from default (Continue). Prevents byte display in Invoke-WebRequest (speeds it up)
 Invoke-WebRequest -Uri $WebUrl -OutFile $tmpFile
 $ProgressPreference = $Pp_old
-
+# Downloaded?
 if (Test-path $tmpFile) {
 	Write-Host "Downloaded: " -NoNewline
 	Write-Host $tmpFile -ForegroundColor Green
+	# Launch it
 	$proc = Start-Process $tmpFile -ArgumentList $arglist -PassThru -Wait -NoNewWindow
+	# Delete it
 	Remove-Item -Path $tmpFile -Force
-
-}
-Else {
+} # Downloaded
+Else { # didn't download
 	Write-Host "Error Downloading: " -NoNewline
 	Write-Host $tmpFile -ForegroundColor red
 	$exitcode=19
-}
+} # didn't download
 Start-Sleep 1
 Write-Host "Done." -ForegroundColor Yellow
 Exit $exitcode
